400f080f82b394b98002748d4f8203083b534637
MD MAMUNUR RASHID <netmamun@yahoo.com>
2018-09-28 12:45:28 -0400

    2nd part done...

--- exercise1.rb
+++ exercise1.rb
@@ -8,21 +8,45 @@ classroom = [
   ["Smokey","Toast","Pacha","Mau"]
 ]
 
+system('clear')
 
+classroom.each_with_index do |item, index|
+      item.each_with_index do |item1,index1|
+          if (item1 == nil)
+            puts "Row #{index+1} seat #{index1+1} is free."
+          end
+      end
+end
 
-
+puts "\n\n"
 
 classroom.each_with_index do |item, index|
 
       item.each_with_index do |item1,index1|
           if (item1 == nil)
             puts "Row #{index+1} seat #{index1+1} is free."
-
+            puts "Do you want to Sit there ? (y/n) : "
+            seat_new = gets.chomp
+              if seat_new == 'y'
+                puts("What's your name: ")
+                new_name = gets.chomp
+                classroom[index][index1] = new_name
+              end
 
 
           end
       end
 end
+
+
+puts "\n"
+classroom.each do |data|
+  puts "#{data}"
+end
+
+
+
+
 
 
 
